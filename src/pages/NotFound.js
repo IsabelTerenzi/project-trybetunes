1d4216b7343d9333740ff66b3838bd39781552f1
Isabel Terenzi <bel.terenzi@gmail.com>
2022-10-28 18:01:09 -0300

    fix

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { FiAlertOctagon } from 'react-icons/fi';
 
 class NotFound extends React.Component {
   render() {
     return (
-      <div data-testid="page-not-found">
+      <div data-testid="page-not-found" className="not-found">
         <h1>Error 404 page not found</h1>
+        <FiAlertOctagon size={ 350 } color="rgb(43, 218, 81)" />
       </div>
     );
   }
